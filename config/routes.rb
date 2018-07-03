41997d41bca914657934019e0006d0ecfca17550
stephanie cattoir <stephanie.cattoir@gmail.com>
2018-07-03 13:07:49 +0200

    removing extra routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'puppies/index'
-  get 'puppies/show'
-  get 'puppies/new'
-  get 'puppies/create'
-  get 'puppies/edit'
-  get 'puppies/update'
-  get 'puppies/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
