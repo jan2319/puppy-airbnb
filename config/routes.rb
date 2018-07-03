ac2207306d3dc057cb9b44a7f24d58303cf32706
stephanie cattoir <stephanie.cattoir@gmail.com>
2018-07-03 12:59:23 +0200

    create puppy_controller + index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'puppies/index'
+  get 'puppies/show'
+  get 'puppies/new'
+  get 'puppies/create'
+  get 'puppies/edit'
+  get 'puppies/update'
+  get 'puppies/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :puppies
 end
